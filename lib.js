f98f04343c36e5f7ad7dfd20a5da72ce30c736c7
Alexander Maẏorov <alexander.majorov@gmail.com>
2017-05-31 17:56:57 +0300

    Create lib.js

---
+++ lib.js
@@ -0,0 +1,16 @@
+save2file = (data, filename) => {
+
+    if(!data) return console.log('No data to save');
+    if(!filename) filename = 'chrome.log.txt';
+    if(typeof data === "object") data = JSON.stringify(data, void 0, 4);
+
+    let blob = new Blob([data], {type: 'text/plain'}),
+        e    = document.createEvent('MouseEvents'),
+        a    = document.createElement('a');
+
+    a.download = filename;
+    a.href = window.URL.createObjectURL(blob);
+    a.dataset.downloadurl =  ['text/plain', a.download, a.href].join(':');
+    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    a.dispatchEvent(e);
+}
